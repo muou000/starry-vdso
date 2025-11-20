22cdfc10231d4da2c15709beb75a33271bc95b53
muou000 <muou000@outlook.com>
2025-11-21 00:06:06 +0800

    add support for embedding vDSO in the ELF.

---
+++ src/embed.rs
@@ -0,0 +1,57 @@
+use core::arch::global_asm;
+
+#[cfg(target_arch = "riscv64")]
+global_asm!("
+    .global vdso_start, vdso_end
+    .section .data
+    .balign 4096
+vdso_start:
+    .incbin \"vdso\\vdso_rv.so\"
+    .balign 4096
+vdso_end:
+
+    .previous
+");
+
+#[cfg(target_arch = "loongarch64")]
+global_asm!("
+    .global vdso_start, vdso_end
+    .section .data
+    .balign 4096
+vdso_start:
+    .incbin \"vdso\\vdso_la.so\"
+    .balign 4096
+vdso_end:
+
+    .previous
+");
+
+#[cfg(target_arch = "aarch64")]
+global_asm!("
+    .global vdso_start, vdso_end
+    .section .data
+    .balign 4096
+vdso_start:
+    .incbin \"vdso\\vdso_aarch.so\"
+    .balign 4096
+vdso_end:
+
+    .previous
+");
+
+#[cfg(target_arch = "x86_64")]
+global_asm!("
+    .global vdso_start, vdso_end
+    .section .data
+    .balign 4096
+vdso_start:
+    .incbin \"vdso\\vdso_x86.so\"
+    .balign 4096
+vdso_end:
+
+    .previous
+");
+
+pub fn embed_vdso() {
+    return;
+}
